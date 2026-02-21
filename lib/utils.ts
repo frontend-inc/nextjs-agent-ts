04207254ab70c0ececc3053a2d49b98745dd278a
Rami Bitar <rbitar@gmail.com>
2026-02-21 15:56:30 -0500

    Update use UUID

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { v4 as uuidv4 } from "uuid"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -10,3 +11,5 @@ export const truncate = (text: string, maxLength: number): string => {
   if (text.length <= maxLength) return text;
   return text.slice(0, maxLength).trim() + '...';
 };
+
+export const generateUUID = (): string => uuidv4();
